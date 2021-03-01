fe750c2d5c80405398c72aab9ea988e69d934f19
Melissa Lawrence <mlawrence537@gmail.com>
2021-03-01 16:21:59 -0500

    commit tree 561c375840953c0606ebb0f4cf2c8e9735591428

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,31 @@
 class Gym
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|membership| membership.gym == self}
+  end
+
+  def lifters 
+    memberships.map {|membership| membership.lifter}
+  end
+
+  def lifter_names
+    lifters.map {|lifter| lifter.name}
+  end
+
+  def lifter_total
+    lifters.map {|lifter| lifter.lift_total}.sum
+  end
+
+
 end
